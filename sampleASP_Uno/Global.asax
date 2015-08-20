4e7317931b1cada29b44028edff154d4c57fb09d
Joe Arcelo <josepharcelo@gmail.com>
2015-08-19 23:14:48 -0500

    first commit

---
+++ sampleASP_Uno/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sampleASP_Uno.MvcApplication" Language="C#" %>
---
+++ sampleASP_Uno/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sampleASP_Uno.Startup))]
+namespace sampleASP_Uno
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
